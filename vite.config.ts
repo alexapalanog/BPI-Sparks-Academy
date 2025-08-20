358c02ad99666068b7e0055eaf64739444bfc79a
alexapalanog <alexapalanog@gmail.com>
2025-08-20 16:28:30 +0800

    vite add

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  // Add this 'base' property
+  base: "/BPI-Sparks-Academy/", 
+  
   plugins: [react()],
 })
