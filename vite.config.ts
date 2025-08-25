94f6c39ce4c410d15badfe83662a0e7c15ea80f5
ericjdz <ericjuliandeguzman77@gmail.com>
2025-08-25 12:54:03 +0800

    fix: set correct base path for vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // Add this 'base' property
-  base: "/BPI-Sparks-Academy/", 
-  
   plugins: [react()],
+  base: "/", // <-- THIS IS THE FIX
 })
